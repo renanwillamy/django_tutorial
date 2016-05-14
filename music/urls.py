618a32939c86114b801403978a4a7e357747851d
Renan Willamy <renanwillamy2@gmail.com>
2016-05-14 00:36:10 -0300

    Alterações na tela Home

--- music/urls.py
+++ music/urls.py
@@ -4,9 +4,8 @@ from . import views
 app_name = 'music'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    #/music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
-    # /music/<album_id>/favorite
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
+
 ]
--- music/views.py
+++ music/views.py
@@ -1,27 +1,17 @@
-from django.shortcuts import render, get_object_or_404
-
-from music.models import Album, Song
-
-
-def index(request):
-    all_albums = Album.objects.all()
-    context = {'all_albums': all_albums}
-    return render(request,'music/index.html',context)
+from django.views import generic
+from . models import Album
 
 
-def detail(request,album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album': album})
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
+
+    def get_queryset(self):
+        return Album.objects.all()
 
 
-def favorite(request,album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except (KeyError,Song.DoesNotExist,ValueError):
-        return render(request, 'music/detail.html',
-               {'album': album, 'error_message':'You did not select a valid song!'})
-    else:
-        selected_song.is_favorite = True
-        selected_song.save()
-        return render(request, 'music/detail.html', {'album': album})
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
+
+
