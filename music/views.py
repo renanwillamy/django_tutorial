4e5c5a42f733047c37766833988117ea4a920253
Renan Willamy <renanwillamy2@gmail.com>
2016-05-16 00:36:09 -0300

    Criacao de Forms

--- music/views.py
+++ music/views.py
@@ -1,5 +1,7 @@
+from django.core.urlresolvers import reverse_lazy
 from django.views import generic
-from . models import Album
+from .models import Album
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 class IndexView(generic.ListView):
@@ -15,3 +17,16 @@ class DetailView(generic.DetailView):
     template_name = 'music/detail.html'
 
 
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
