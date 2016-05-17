5fe9219e7918b35c9b52e3e82e87ae56a25e8b86
Renan Willamy <renanwillamy2@gmail.com>
2016-05-16 22:58:51 -0300

    Finalização do tutorial ate o video 37

--- music/views.py
+++ music/views.py
@@ -1,6 +1,10 @@
 from django.core.urlresolvers import reverse_lazy
 from django.views import generic
 from .models import Album
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.views.generic import View
+from .forms import UserForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
@@ -30,3 +34,34 @@ class AlbumUpdate(UpdateView):
 class AlbumDelete(DeleteView):
     model = Album
     success_url = reverse_lazy('music:index')
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            # return user object if credentials are correct
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active():
+                    login(request, user)
+                    redirect('music:index')
+
+        return render(request, self.template_name, {'form': form})
